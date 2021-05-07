d3f000f2d435d71c59dbe3518789ebddd1503c00
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-05-07 20:38:52 +0800

    add decodeURL

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-let search = document.URL.split("#")[1];
+let search = decodeURI(document.URL.split("#")[1]);
 if (search !== undefined) {
-    document.getElementById("search-input").value = search
+    document.getElementById("search-input").value = search;
     setTimeout(() => {
-        document.querySelector("#cursor img").src = "./image/cursor-input.png"
-    }, 3000)
+        document.querySelector("#cursor img").src = "./image/cursor-input.png";
+    }, 3000);
     setTimeout(() => {
-        document.getElementById("search-box").style = "animation: input 3s;width: 0;"
-    }, 3000)
+        document.getElementById("search-box").style = "animation: input 3s;width: 0;";
+    }, 3000);
     setTimeout(() => {
-        window.location = `https://www.google.com/search?q=${search}`
-    }, 6000)
+        window.location = `https://www.google.com/search?q=${search}`;
+    }, 6000);
 }
